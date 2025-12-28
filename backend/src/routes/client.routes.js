f85322bae5eb84f26efb7739a0122703ab3d16d2
Heeralal Kumar Heera <heeralalkumarheera@gmail.com>
2025-12-28 21:01:52 +0530

    Day 5: Client management CRUD APIs completed

--- backend/src/routes/client.routes.js
+++ backend/src/routes/client.routes.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-const { getClients } = require("../controllers/client.controller");
+const {
+  createClient,
+  getAllClients,
+  updateClient,
+  deleteClient,
+} = require("../controllers/client.controller");
 
-router.get("/", getClients);
+const {
+  protect,
+  authorizeRoles,
+} = require("../middlewares/auth.middleware");
+
+// CREATE CLIENT (admin + sales)
+router.post(
+  "/",
+  protect,
+  authorizeRoles("admin", "sales"),
+  createClient
+);
+
+// GET ALL CLIENTS (all logged users)
+router.get("/", protect, getAllClients);
+
+// UPDATE CLIENT (admin + sales)
+router.put(
+  "/:id",
+  protect,
+  authorizeRoles("admin", "sales"),
+  updateClient
+);
+
+// DELETE CLIENT (admin only)
+router.delete(
+  "/:id",
+  protect,
+  authorizeRoles("admin"),
+  deleteClient
+);
 
 module.exports = router;
